0d0ce4d3ea34d403bfdf0e73b382d4d47887dd8d
Thedath Oudarya <thedath.k@umcg.global>
2020-10-14 13:25:45 +0530

    Created the react app

---
+++ build/precache-manifest.57ef8fbfacdd9c356be9f531a44560ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c3ac2b217ec9b1a78a532e85dab9c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eefbe25d0343e03dbf60",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e30ca9989399ddf05333",
+    "url": "/static/js/2.f4c143d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f4c143d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eefbe25d0343e03dbf60",
+    "url": "/static/js/main.eb669309.chunk.js"
+  },
+  {
+    "revision": "20555ec55c814b5dc65e",
+    "url": "/static/js/runtime-main.e4de621e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
